ba667c4ff55451433806b36e2403e6294f85f77c
José Simões <josesimoes@users.noreply.github.com>
2018-11-15 10:45:04 +0000

    Switch VS extension download to MyGet
    - VS marketplace is blocking downloads from AppVeyor (HTTP 412).

--- install-vsix-appveyor.ps1
+++ install-vsix-appveyor.ps1
@@ -1,5 +1,5 @@
 $vsixPath = "$($env:USERPROFILE)\nanoFramework.Tools.VS2017.Extension.vsix"
-(New-Object Net.WebClient).DownloadFile('https://marketplace.visualstudio.com/_apis/public/gallery/publishers/vs-publisher-1470366/vsextensions/nanoFrameworkVS2017Extension/0/vspackage', $vsixPath)
+(New-Object Net.WebClient).DownloadFile('https://www.myget.org/F/nanoframework-dev/vsix/47973986-ed3c-4b64-ba40-a9da73b44ef7-1.0.1.0.vsix', $vsixPath)
 "`"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
 
 'Installing nanoFramework VS extension ...' | Write-Host -ForegroundColor White -NoNewline
