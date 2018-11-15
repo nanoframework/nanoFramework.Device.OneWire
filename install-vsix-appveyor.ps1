96f9ddcd599c59b6c4996471a78af07506dc9e49
José Simões <josesimoes@users.noreply.github.com>
2018-11-15 10:55:24 +0000

    Update install-vsix-appveyor.ps1

--- install-vsix-appveyor.ps1
+++ install-vsix-appveyor.ps1
@@ -1,4 +1,4 @@
-$vsixPath = "$($env:USERPROFILE)\nanoFramework.Tools.VS2017.Extension.vsix"
+$vsixPath = "$($env:USERPROFILE)\47973986-ed3c-4b64-ba40-a9da73b44ef7-1.0.1.0.vsix"
 (New-Object Net.WebClient).DownloadFile('https://www.myget.org/F/nanoframework-dev/vsix/47973986-ed3c-4b64-ba40-a9da73b44ef7-1.0.1.0.vsix', $vsixPath)
 "`"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
 
