1fa7459ba8a9fe2cf390914a45817c789a2bc567
nfbot <nfbot@users.noreply.github.com>
2018-11-28 00:14:04 +0000

    Switch to Azure Pipelines (#3)

--- install-vsix-appveyor.ps1
+++
@@ -1,9 +0,0 @@
-$vsixPath = "$($env:USERPROFILE)\47973986-ed3c-4b64-ba40-a9da73b44ef7-1.0.1.0.vsix"
-(New-Object Net.WebClient).DownloadFile('https://www.myget.org/F/nanoframework-dev/vsix/47973986-ed3c-4b64-ba40-a9da73b44ef7-1.0.1.0.vsix', $vsixPath)
-"`"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe`" /q /a $vsixPath" | out-file ".\install-vsix.cmd" -Encoding ASCII
-
-'Installing nanoFramework VS extension ...' | Write-Host -ForegroundColor White -NoNewline
-
-& .\install-vsix.cmd > $null
-
-'OK' | Write-Host -ForegroundColor Green
