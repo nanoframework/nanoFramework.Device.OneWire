9c29859c93d18afe5852d0ba1759d73fe0ea8e1e
José Simões <jose.simoes@eclo.solutions>
2021-11-24 15:59:06 +0000

    Work CI-CD
    - Update azure pipeline yaml.
    - Fix nuspec.
    - Update logo for nuget assets.
    - Switch order to nuget feeds in nuget.config.
    - Update projects to migrate to VS2022.
    - Fix typos.
    - Rename several repo files.
    
    ***NO_CI***

--- nanoFramework.Devices.OneWire/Properties/AssemblyInfo.cs
+++ nanoFramework.Devices.OneWire/Properties/AssemblyInfo.cs
@@ -5,21 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CSharp.BlankApplication")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CSharp.BlankApplication")]
-[assembly: AssemblyCopyright("Copyright © ")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyTitle("nanoFramework.Devices.OneWire")]
+[assembly: AssemblyCompany("nanoFramework Contributors")]
+[assembly: AssemblyProduct("nanoFramework.Devices.OneWire")]
+[assembly: AssemblyCopyright("Copyright (c) .NET Foundation and Contributors")]
 
 ////////////////////////////////////////////////////////////////
 // update this whenever the native assembly signature changes //
 [assembly: AssemblyNativeVersion("100.0.3.4")]
 ////////////////////////////////////////////////////////////////
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
